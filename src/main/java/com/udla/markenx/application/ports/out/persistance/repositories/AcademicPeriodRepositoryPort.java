43895fa059f3e0415190d812f765751a8ec1b516
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-04 18:08:55 -0500

    feat: Add AcademicPeriodRepositoryPort interface and implement AcademicPeriodSeeder for database seeding

---
+++ src/main/java/com/udla/markenx/application/ports/out/persistance/repositories/AcademicPeriodRepositoryPort.java
@@ -0,0 +1,7 @@
+package com.udla.markenx.application.ports.out.persistance.repositories;
+
+import com.udla.markenx.core.models.AcademicPeriod;
+
+public interface AcademicPeriodRepositoryPort {
+  AcademicPeriod createAcademicPeriod(AcademicPeriod period);
+}
---
+++ src/main/java/com/udla/markenx/application/seeders/AcademicPeriodSeeder.java
@@ -0,0 +1,35 @@
+package com.udla.markenx.application.seeders;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.udla.markenx.core.models.AcademicPeriod;
+
+import com.udla.markenx.application.factories.RandomAcademicPeriodFactory;
+import com.udla.markenx.application.ports.out.persistance.repositories.AcademicPeriodRepositoryPort;
+
+@Component
+@Profile("dev")
+public class AcademicPeriodSeeder implements CommandLineRunner {
+
+	private final RandomAcademicPeriodFactory factory;
+	private final AcademicPeriodRepositoryPort repository;
+
+	public AcademicPeriodSeeder(
+			RandomAcademicPeriodFactory builder,
+			AcademicPeriodRepositoryPort repository) {
+		this.factory = builder;
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Starting database seeding...");
+
+		AcademicPeriod period = factory.createRandomAcademicPeriod(null, null);
+		repository.createAcademicPeriod(period);
+
+		System.out.println("Database seeding complete!");
+	}
+}
