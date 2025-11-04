229e9d894cb06000e4f3982d761d1e2a3aeb3a09
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-03 21:11:46 -0500

    feat: Se agrega estado al intento

--- src/main/java/com/udla/markenx/core/models/Attempt.java
+++ src/main/java/com/udla/markenx/core/models/Attempt.java
@@ -3,6 +3,8 @@ package com.udla.markenx.core.models;
 import java.time.Duration;
 import java.time.LocalDate;
 
+import com.udla.markenx.core.valueobjects.enums.AttemptStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,17 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Attempt {
-    private Long id;
-    private double score;
-    private LocalDate date;
-    private Duration duration;
+	private Long id;
+	private double score;
+	private LocalDate date;
+	private Duration duration;
+	private AttemptStatus status;
 
-    public Attempt(
-            double score,
-            LocalDate date,
-            Duration duration) {
-        this.score = score;
-        this.date = date;
-        this.duration = duration;
-    }
+	public Attempt(
+			double score,
+			LocalDate date,
+			Duration duration) {
+		this.score = score;
+		this.date = date;
+		this.duration = duration;
+	}
 }
