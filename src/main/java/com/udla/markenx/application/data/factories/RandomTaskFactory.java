6bfbad4c5f67594f9060902ee18234986de88bcc
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-03 21:48:00 -0500

    refactor: Se pasa las fabricas a la capa de aplicación

--- src/main/java/com/udla/markenx/application/data/factories/RandomTaskFactory.java
+++ src/main/java/com/udla/markenx/application/data/factories/RandomTaskFactory.java
@@ -1,4 +1,4 @@
-package com.udla.markenx.infrastructure.out.persistance.database.utils.factories;
+package com.udla.markenx.application.data.factories;
 
 import org.springframework.stereotype.Component;
 
