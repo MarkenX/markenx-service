8a086bbf9fc59bc0ae8c87c7b22cc929fa0ecc66
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-04 19:21:53 -0500

    feat: Add relationships in CourseJpaEntity for assignments, students, and academic period

--- src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/CourseJpaEntity.java
+++ src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/CourseJpaEntity.java
@@ -1,24 +1,41 @@
 package com.udla.markenx.infrastructure.out.persistance.repositories.jpa.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "courses")
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "courses")
 public class CourseJpaEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "course_id")
   private Long id;
+
+  @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<AssignmentJpaEntity> assignments;
+
+  @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<StudentJpaEntity> students;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "period_id")
+  private AcademicPeriodJpaEntity academicPeriod;
 }
