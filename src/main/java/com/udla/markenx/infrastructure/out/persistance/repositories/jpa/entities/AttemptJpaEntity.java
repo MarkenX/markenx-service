5e1c3dcaf1a3d4c4fab88bf6ce81e5374d654a3a
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-05 17:40:50 -0500

    feat: Add missing @Enumerated annotations for AttemptResult and AttemptStatus fields in AttemptJpaEntity

--- src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/AttemptJpaEntity.java
+++ src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/AttemptJpaEntity.java
@@ -9,6 +9,8 @@ import com.udla.markenx.core.valueobjects.enums.AttemptStatus;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -40,9 +42,11 @@ public class AttemptJpaEntity {
 	@Column(name = "attempt_duration")
 	private Duration duration;
 
+	@Enumerated(EnumType.STRING)
 	@Column(name = "attempt_result")
 	private AttemptResult result;
 
+	@Enumerated(EnumType.STRING)
 	@Column(name = "attempt_current_status")
 	private AttemptStatus currentStatus;
 
