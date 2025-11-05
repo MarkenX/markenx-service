c1cce2e508c7524bc4ca22ddd9f3a190a1052fac
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-04 19:18:02 -0500

    feat: Enhance AcademicPeriodJpaEntity with additional fields and constructors for better data representation

--- src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/AcademicPeriodJpaEntity.java
+++ src/main/java/com/udla/markenx/infrastructure/out/persistance/repositories/jpa/entities/AcademicPeriodJpaEntity.java
@@ -1,12 +1,18 @@
 package com.udla.markenx.infrastructure.out.persistance.repositories.jpa.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "academic-period")
 public class AcademicPeriodJpaEntity {
@@ -14,4 +20,27 @@ public class AcademicPeriodJpaEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "period_id")
   private Long id;
+
+  @Column(name = "period_startDate")
+  @Temporal(TemporalType.DATE)
+  private LocalDate startDate;
+
+  @Column(name = "period_endDate")
+  @Temporal(TemporalType.DATE)
+  private LocalDate endDate;
+
+  @Column(name = "period_label")
+  private String label;
+
+  @OneToMany(mappedBy = "academicPeriod", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<CourseJpaEntity> courses = new ArrayList<>();
+
+  public AcademicPeriodJpaEntity(
+      LocalDate startDate,
+      LocalDate endDate,
+      String label) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.label = label;
+  }
 }
