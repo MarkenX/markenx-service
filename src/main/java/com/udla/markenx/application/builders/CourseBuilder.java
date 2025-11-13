3a35eb079805904b49bbe6a9a5e2bd7dc1a0ff45
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-13 17:25:21 -0500

    feat: Implement CourseBuilder for course creation and management

---
+++ src/main/java/com/udla/markenx/application/builders/CourseBuilder.java
@@ -0,0 +1,53 @@
+package com.udla.markenx.application.builders;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import com.udla.markenx.application.ports.out.data.generators.random.RandomCourseDataGenerator;
+import com.udla.markenx.core.models.Course;
+
+@Component
+public class CourseBuilder {
+
+  private final RandomCourseDataGenerator randomGenerator;
+
+  private UUID academicTermId;
+  private int academicTermYear;
+  private String name;
+
+  public CourseBuilder reset() {
+    this.academicTermId = null;
+    this.academicTermYear = -1;
+    this.name = null;
+    return this;
+  }
+
+  public CourseBuilder(RandomCourseDataGenerator randomGenerator) {
+    this.randomGenerator = randomGenerator;
+  }
+
+  public CourseBuilder setAcademicTermId(UUID academicTermId) {
+    this.academicTermId = academicTermId;
+    return this;
+  }
+
+  public CourseBuilder setAcademicTermYear(int academicTermYear) {
+    this.academicTermYear = academicTermYear;
+    return this;
+  }
+
+  public CourseBuilder setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public CourseBuilder randomName() {
+    this.name = randomGenerator.name();
+    return this;
+  }
+
+  public Course build() {
+    return new Course(academicTermId, academicTermYear, name);
+  }
+}
