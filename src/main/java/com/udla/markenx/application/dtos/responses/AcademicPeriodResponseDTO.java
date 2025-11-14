8d0df244bf26bdc511d567ba1ba5fa5c99057aa9
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-13 21:55:10 -0500

    refactor: Convert AcademicPeriodResponseDTO to a record for improved simplicity and immutability

--- src/main/java/com/udla/markenx/application/dtos/responses/AcademicPeriodResponseDTO.java
+++ src/main/java/com/udla/markenx/application/dtos/responses/AcademicPeriodResponseDTO.java
@@ -2,15 +2,10 @@ package com.udla.markenx.application.dtos.responses;
 
 import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class AcademicPeriodResponseDTO {
-  private final String code;
-  private final LocalDate startOfTerm;
-  private final LocalDate endOfTerm;
-  private final int academicYear;
-  private final String label;
+public record AcademicPeriodResponseDTO(
+    String code,
+    LocalDate startOfTerm,
+    LocalDate endOfTerm,
+    int academicYear,
+    String label) {
 }
