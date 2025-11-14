80cf0f5aaa5f08676fa1196843fdb4db2cc7148f
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-13 21:55:52 -0500

    refactor: Convert CourseResponseDTO to a record for improved simplicity and immutability

--- src/main/java/com/udla/markenx/application/dtos/responses/CourseResponseDTO.java
+++ src/main/java/com/udla/markenx/application/dtos/responses/CourseResponseDTO.java
@@ -1,11 +1,6 @@
 package com.udla.markenx.application.dtos.responses;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class CourseResponseDTO {
-  private final String code;
-  private final String name;
+public record CourseResponseDTO(
+    String code,
+    String name) {
 }
