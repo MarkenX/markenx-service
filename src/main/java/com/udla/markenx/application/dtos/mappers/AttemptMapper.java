d4580817c04ba1536d94c8f095a6dff71ab1739f
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-13 22:01:13 -0500

    refactor: Update AttemptResponseDTO to use 'code' instead of 'id' and adjust AttemptMapper accordingly

--- src/main/java/com/udla/markenx/application/dtos/mappers/AttemptMapper.java
+++ src/main/java/com/udla/markenx/application/dtos/mappers/AttemptMapper.java
@@ -4,15 +4,17 @@ import com.udla.markenx.application.dtos.responses.AttemptResponseDTO;
 import com.udla.markenx.core.models.Attempt;
 
 public class AttemptMapper {
-  public static AttemptResponseDTO toDto(Attempt attempt) {
-    if (attempt == null)
+  public static AttemptResponseDTO toDto(Attempt domain) {
+    if (domain == null)
       return null;
 
     AttemptResponseDTO dto = new AttemptResponseDTO(
-        attempt.getId(),
-        attempt.getScore(),
-        attempt.getSubmittedAt(),
-        attempt.getTimeSpent());
+        domain.getCode(),
+        domain.getScore(),
+        domain.getCreatedAtDate(),
+        domain.getTimeSpent(),
+        domain.getAttemptStatus(),
+        domain.getResult());
 
     return dto;
   }
--- src/main/java/com/udla/markenx/application/dtos/responses/AttemptResponseDTO.java
+++ src/main/java/com/udla/markenx/application/dtos/responses/AttemptResponseDTO.java
@@ -3,5 +3,14 @@ package com.udla.markenx.application.dtos.responses;
 import java.time.Duration;
 import java.time.LocalDate;
 
-public record AttemptResponseDTO(Long id, double score, LocalDate date, Duration duration) {
+import com.udla.markenx.core.valueobjects.enums.AttemptResult;
+import com.udla.markenx.core.valueobjects.enums.AttemptStatus;
+
+public record AttemptResponseDTO(
+    String code,
+    double score,
+    LocalDate submittedAt,
+    Duration timeSpent,
+    AttemptStatus status,
+    AttemptResult result) {
 }
