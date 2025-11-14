e4dee5ceaec091ed41e09d9d9d01cc6bdc11ef02
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-13 21:26:23 -0500

    style: Format code in AssignmentStatusMapper for improved readability

--- src/main/java/com/udla/markenx/application/dtos/mappers/AssignmentStatusMapper.java
+++ src/main/java/com/udla/markenx/application/dtos/mappers/AssignmentStatusMapper.java
@@ -8,13 +8,13 @@ import com.udla.markenx.application.dtos.responses.AssignmentStatusResponseDTO;
 import com.udla.markenx.core.valueobjects.enums.AssignmentStatus;
 
 public class AssignmentStatusMapper {
-    public static AssignmentStatusResponseDTO toDto(AssignmentStatus status) {
-        return new AssignmentStatusResponseDTO(status.name(), status.getLabel());
-    }
+  public static AssignmentStatusResponseDTO toDto(AssignmentStatus status) {
+    return new AssignmentStatusResponseDTO(status.name(), status.getLabel());
+  }
 
-    public static List<AssignmentStatusResponseDTO> toDtoList() {
-        return Arrays.stream(AssignmentStatus.values())
-                .map(AssignmentStatusMapper::toDto)
-                .collect(Collectors.toList());
-    }
+  public static List<AssignmentStatusResponseDTO> toDtoList() {
+    return Arrays.stream(AssignmentStatus.values())
+        .map(AssignmentStatusMapper::toDto)
+        .collect(Collectors.toList());
+  }
 }
