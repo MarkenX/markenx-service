8d0c9ab385fa9053896e5bdb0a15189981738799
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-13 21:46:06 -0500

    refactor: Simplify CourseDtoMapper and CourseResponseDTO by removing unnecessary fields and constructors for a cleaner response structure

--- src/main/java/com/udla/markenx/application/dtos/mappers/CourseDtoMapper.java
+++ src/main/java/com/udla/markenx/application/dtos/mappers/CourseDtoMapper.java
@@ -5,22 +5,13 @@ import com.udla.markenx.core.models.Course;
 
 public class CourseDtoMapper {
 
-  public static CourseResponseDTO toResponseDto(Course course) {
-    if (course == null)
+  public static CourseResponseDTO toResponseDto(Course domain) {
+    if (domain == null)
       return null;
 
-    CourseResponseDTO dto = new CourseResponseDTO();
-    dto.setId(course.getId());
-    dto.setAssignmentsCount(course.getAssignments().size());
-    dto.setStudentsCount(course.getStudents().size());
-    dto.setAcademicPeriodId(course.getAcademicTermId());
-    dto.setLabel(course.getName());
-
-    dto.setCreatedBy(course.getCreatedBy());
-    dto.setCreatedAt(course.getCreatedAtDateTime());
-    dto.setLastModifiedBy(course.getLastModifiedBy());
-    dto.setLastModifiedAt(course.getUpdatedAtDateTime());
-
+    CourseResponseDTO dto = new CourseResponseDTO(
+        domain.getCode(),
+        domain.getName());
     return dto;
   }
 }
--- src/main/java/com/udla/markenx/application/dtos/responses/CourseResponseDTO.java
+++ src/main/java/com/udla/markenx/application/dtos/responses/CourseResponseDTO.java
@@ -1,25 +1,11 @@
 package com.udla.markenx.application.dtos.responses;
 
-import java.time.LocalDateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class CourseResponseDTO {
-  private Long id;
-  private int assignmentsCount;
-  private int studentsCount;
-  private Long academicPeriodId;
-  private String label;
-
-  private String createdBy;
-  private LocalDateTime createdAt;
-  private String lastModifiedBy;
-  private LocalDateTime lastModifiedAt;
+  private final String code;
+  private final String name;
 }
