8703c14e7790073e888985e8c3c8ca913211da12
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-17 15:57:01 -0500

    feat: Enhance MainFactor enum to include descriptions for each factor

--- src/main/java/com/udla/markenx/game/domain/valueobject/MainFactor.java
+++ src/main/java/com/udla/markenx/game/domain/valueobject/MainFactor.java
@@ -1,18 +1,24 @@
 package com.udla.markenx.game.domain.valueobject;
 
 public enum MainFactor {
-  PSYCHOLOGICAL("Psicológico"),
-  PERSONAL("Personal"),
-  SOCIAL("Social"),
-  CULTURAL("Cultural");
+  PSYCHOLOGICAL("Psicológico", "Descripción del factor psicológico"),
+  PERSONAL("Personal", "Descripción del factor personal"),
+  SOCIAL("Social", "Descripción del factor social"),
+  CULTURAL("Cultural", "Descripción del factor cultural");
 
   private final String label;
+  private final String description;
 
-  MainFactor(String label) {
+  MainFactor(String label, String description) {
     this.label = label;
+    this.description = description;
   }
 
   public String getLabel() {
     return label;
+  }
+
+  public String getDescription() {
+    return this.description;
   }
 }
