6e4f0f4737c02db08e430d5603e843cf96bfaa85
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-17 16:24:23 -0500

    feat: Implement Action class with properties and validation

--- src/main/java/com/udla/markenx/game/domain/model/Action.java
+++ src/main/java/com/udla/markenx/game/domain/model/Action.java
@@ -1,5 +1,52 @@
 package com.udla.markenx.game.domain.model;
 
-public class Action {
+import java.util.UUID;
 
+import com.udla.markenx.game.domain.valueobject.ActionCategory;
+import com.udla.markenx.shared.domain.util.validator.EntityValidator;
+
+public class Action {
+  private static final Class<Action> CLAZZ = Action.class;
+
+  private final UUID id;
+  private final ActionCategory category;
+  private final String name;
+  private final String description;
+  private final Double cost;
+
+  public Action(UUID id, ActionCategory category, String name, String description, Double cost) {
+    this.id = requireId(id);
+    this.category = category;
+    this.name = name;
+    this.description = description;
+    this.cost = cost;
+  }
+
+  // #region Getters
+
+  public UUID getId() {
+    return this.id;
+  }
+
+  public ActionCategory getCategory() {
+    return this.category;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public Double getCost() {
+    return this.cost;
+  }
+
+  // #endregion
+
+  private UUID requireId(UUID id) {
+    return EntityValidator.ensureNotNull(CLAZZ, id, "id");
+  }
 }
