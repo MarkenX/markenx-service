8eed846c4f81dca2bbe545313f5e665f7509b7d2
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-18 10:43:16 -0500

    feat: Implement PlayerActionImpact class with properties and validation

---
+++ src/main/java/com/udla/markenx/game/domain/model/player/PlayerActionImpact.java
@@ -0,0 +1,54 @@
+package com.udla.markenx.game.domain.model.player;
+
+import java.util.UUID;
+
+import com.udla.markenx.game.domain.model.consumer.Subfactor;
+import com.udla.markenx.shared.domain.util.validator.EntityValidator;
+
+public class PlayerActionImpact {
+  private static final Class<PlayerActionImpact> CLAZZ = PlayerActionImpact.class;
+
+  private final UUID id;
+  private final PlayerAction playerAction;
+  private final Subfactor subfactor;
+  private final int impact;
+
+  public PlayerActionImpact(
+      UUID id,
+      PlayerAction playerAction,
+      Subfactor subfactor,
+      int impact) {
+    this.id = id;
+    this.playerAction = playerAction;
+    this.subfactor = subfactor;
+    this.impact = impact;
+  }
+
+  // #region Getters
+
+  public UUID getId() {
+    return this.id;
+  }
+
+  public PlayerAction getPlayerAction() {
+    return this.playerAction;
+  }
+
+  public Subfactor getSubfactor() {
+    return this.subfactor;
+  }
+
+  public int getImpact() {
+    return this.impact;
+  }
+
+  // #endregion
+
+  // #region Validations
+
+  private UUID requireId(UUID id) {
+    return EntityValidator.ensureNotNull(CLAZZ, id, "id");
+  }
+
+  // #endregion
+}
