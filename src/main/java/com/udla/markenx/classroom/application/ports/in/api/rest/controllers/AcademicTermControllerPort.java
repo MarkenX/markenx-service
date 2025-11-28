d2fa1786ff5321230769c0d3636ecf2fbbd648b2
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2025-11-28 09:57:52 -0500

    feat: Refactor getAllAcademicTerms method signature for consistency

--- src/main/java/com/udla/markenx/classroom/application/ports/in/api/rest/controllers/AcademicTermControllerPort.java
+++ src/main/java/com/udla/markenx/classroom/application/ports/in/api/rest/controllers/AcademicTermControllerPort.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import com.udla.markenx.classroom.application.dtos.requests.AcademicPeriod.CreateAcademicTermRequestDTO;
 import com.udla.markenx.classroom.application.dtos.requests.AcademicPeriod.UpdateAcademicTermRequestDTO;
 import com.udla.markenx.classroom.application.dtos.responses.AcademicPeriodResponseDTO;
+import com.udla.markenx.shared.domain.valueobjects.DomainBaseModelStatus;
 
 public interface AcademicTermControllerPort {
 
@@ -20,9 +21,7 @@ public interface AcademicTermControllerPort {
 
   ResponseEntity<AcademicPeriodResponseDTO> getAcademicTermById(UUID id);
 
-  ResponseEntity<Page<AcademicPeriodResponseDTO>> getAllAcademicTerms(
-      com.udla.markenx.shared.domain.valueobjects.DomainBaseModelStatus status,
-      Pageable pageable);
+  ResponseEntity<Page<AcademicPeriodResponseDTO>> getAllAcademicTerms(DomainBaseModelStatus status, Pageable pageable);
 
   ResponseEntity<Void> disableAcademicTerm(UUID id);
 
