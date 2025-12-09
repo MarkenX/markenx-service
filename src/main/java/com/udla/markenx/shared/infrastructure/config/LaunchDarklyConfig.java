e21e53010996408d4585e5e1073c6e613516a8a0
DeividN <david.navarrete.baraja@udla.edu.ec>
2025-12-08 19:57:58 -0500

    fix: LauchDarkly KEY

--- src/main/java/com/udla/markenx/shared/infrastructure/config/LaunchDarklyConfig.java
+++ src/main/java/com/udla/markenx/shared/infrastructure/config/LaunchDarklyConfig.java
@@ -1,15 +1,18 @@
 package com.udla.markenx.shared.infrastructure.config;
 
 import com.launchdarkly.sdk.server.LDClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LaunchDarklyConfig {
 
+  @Value("${launchdarkly.sdk-key:sdk-9177cd48-da72-4f44-9d5e-4b5105478d5b}")
+  private String sdkKey;
+
   @Bean
   public LDClient ldClient() {
-    String sdkKey = System.getenv("LAUNCHDARKLY_SDK_KEY");
     LDClient client = new LDClient(sdkKey);
 
     if (!client.isInitialized()) {
